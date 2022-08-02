8b5434fc92e69b4d478879be7143d9db927b0086
lakotelman <la.n.kot@pm.me>
2022-08-02 15:38:52 -0800

    removed node modules?

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+
+
+
+app.set("view engine", "ejs");
+app.set("views", "./templates/views/");
+
+app.listen(port, () => {
+  console.log(`Express application started at port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/profile", (req, res) => {
+  res.render("profile.ejs");
+});
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+app.get("/user", (req, res) => {
+  res.render("user.ejs");
+});
